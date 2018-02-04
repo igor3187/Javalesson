e892b69c50917e9cd4ffba870e38296608d2a2bf
levit <levit.igor01@gmail.com>
2018-02-04 21:22:31 +0200

    С помощью класса Scanner ввести целое число. Если это число от 0 до 10, вывести на консоль сообщение “Положительное число меньше 10”, иначе вывести сообщение “Положительное число больше 10 или отрицательное”. Используем оператор if-else.

---
+++ IfElse1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class IfElse1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.print("Enter number : ");
+        x = scan.nextInt();
+        if (x <= 10) {
+            System.out.println("Number is less then 10 or equal to.");
+        } else {
+            System.out.println("Number is greater than 10 or it's negative.");
+
+        }
+    }
+}
