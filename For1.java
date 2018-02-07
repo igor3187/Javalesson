a1c11b99eb0af32608a92c2d884cd8ff3f8f9630
levit <levit.igor01@gmail.com>
2018-02-07 21:09:39 +0200

    Распечатать 5 символов в одну строку начиная с ‘h’. Используем цикл for.

---
+++ For1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class For1 {
+    public static void main(String[] args) {
+        char h;
+        for ( h = 0; h < 5; h++ );
+        System.out.println((char) + h);
+    }
+}
